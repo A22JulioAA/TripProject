85ea0030e88c40fe24a84e2ad8385e255a7029c0
Xulio <a22julioaa@iessanclemente.net>
2024-11-25 12:55:44 +0100

    feature(navegacion): añadida navegacion entre todas las pantallas

--- app/src/main/java/com/example/tripproject/MainActivity.kt
+++ app/src/main/java/com/example/tripproject/MainActivity.kt
@@ -1,19 +1,28 @@
 package com.example.tripproject
 
-import android.content.res.Resources.Theme
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Check
 import androidx.compose.material.icons.filled.Edit
 import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.BottomAppBar
 import androidx.compose.material3.BottomAppBarDefaults
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.FloatingActionButtonDefaults
@@ -26,13 +35,21 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.example.tripproject.ui.theme.TealLight
 import com.example.tripproject.ui.theme.TripProjectTheme
 
@@ -43,67 +60,215 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             TripProjectTheme {
+
+                val  navController = rememberNavController()
+
                 Scaffold(
                     modifier = Modifier.fillMaxSize(),
-                    topBar = { topBar() },
-                    bottomBar = {
-                        BottomAppBar (
-                            actions = {
-                                IconButton(onClick = {}) {
-                                    Icon(Icons.Filled.Home, contentDescription = "Hola")
-                                }
-                                IconButton(onClick = {}) {
-                                    Icon(Icons.Filled.Edit, contentDescription = "Hola")
-                                }
-                                IconButton(onClick = {}) {
-                                    Icon(Icons.Filled.Check, contentDescription = "Hola")
-                                }
-                            },
-                            floatingActionButton = {
-                                FloatingActionButton(
-                                    onClick = {},
-                                    containerColor = BottomAppBarDefaults.bottomAppBarFabColor,
-                                    elevation = FloatingActionButtonDefaults.bottomAppBarFabElevation()
-                                ) {
-                                    Icon(Icons.Filled.Add, "Add supoño")
-                                }
+                    topBar = { TopBar() },
+                    bottomBar = { BottomBar(navController) },
+                    content = { innerPadding ->
+                        NavHost(navController = navController, startDestination = "home") {
+                            composable("home") {
+                                MyApp(modifier = Modifier.padding(innerPadding))
                             }
-                        )
-                    },
-                    ) { innerPadding ->
-                        MyApp (modifier = Modifier.padding(innerPadding))
-                }
+                            composable("conversorDivisasScreen") {
+                                ConversorDivisasScreen(modifier = Modifier.padding(innerPadding))
+                            }
+                            composable("gastosViajeScreen") {
+                                GastosViajeScreen(modifier = Modifier.padding(innerPadding))
+                            }
+                            composable("mapasYRutasScreen") {
+                                MapasYRutasScreen(modifier = Modifier.padding(innerPadding))
+                            }
+                        }
+                    }
+                    )
             }
         }
     }
 }
 
+// Función para crear la top bar
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun topBar(){
+fun TopBar(){
+        var expanded by remember { mutableStateOf(false) }
+
         TopAppBar(
             colors = TopAppBarDefaults.topAppBarColors(
                 containerColor = MaterialTheme.colorScheme.primaryContainer,
                 titleContentColor = MaterialTheme.colorScheme.primary
             ),
             title = {
-                Icon(
-                    Icons.Filled.Home, contentDescription = "Hola"
-                )
+                Row (
+                    modifier = Modifier.fillMaxSize(),
+                    horizontalArrangement = Arrangement.SpaceBetween,
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Image(
+                        painter = painterResource(id = R.drawable.logo),
+                        contentDescription = "Logo app",
+                        modifier = Modifier.size(48.dp).weight(1f)
+                    )
+                    Text(
+                        text = stringResource(id = R.string.topbar_title),
+                        style = MaterialTheme.typography.titleMedium,
+                        modifier = Modifier.weight(1f)
+                    )
+                    Image(
+                        painter = painterResource(id = R.drawable.user_icon),
+                        contentDescription = "Imagen usuario",
+                        modifier = Modifier
+                            .size(32.dp)
+                            .weight(1f)
+                            .clickable { expanded = !expanded }
+                    )
+
+                }
+
             }
         )
 
+        DropdownMenu(
+            expanded = expanded,
+            onDismissRequest = { expanded = false }
+        ) {
+            DropdownMenuItem(onClick = {} , text = { MyTxt("1", Modifier.padding(15.dp)) })
+            DropdownMenuItem(onClick = {} , text = { MyTxt("2", Modifier.padding(15.dp)) })
+            DropdownMenuItem(onClick = {} , text = { MyTxt("3", Modifier.padding(15.dp)) })
+
+        }
 }
 
+// Función para crear la bottom bar
+@Composable
+fun BottomBar (navController: NavController) {
+    BottomAppBar (
+    ) {
+        Row (
+            modifier = Modifier.fillMaxSize(),
+            horizontalArrangement = Arrangement.SpaceEvenly,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            IconButton(
+                onClick = { navController.navigate("home") },
+                modifier = Modifier.weight(1f)
+            ) {
+                Image(
+                    painter = painterResource(R.drawable.home_logo_bottombar),
+                    contentDescription = "Home logo",
+                    modifier = Modifier.size(64.dp)
+                )
+            }
+            IconButton(
+                onClick = { navController.navigate("gastosViajeScreen")},
+                modifier = Modifier.weight(1f)
+            ) {
+                Image(
+                    painter = painterResource(R.drawable.car_logo_bottombar),
+                    contentDescription = "Home logo",
+                    modifier = Modifier.size(64.dp)
+                )
+            }
+            IconButton(
+                onClick = { navController.navigate("conversorDivisasScreen") },
+                modifier = Modifier.weight(1f)
+            ) {
+                Image(
+                    painter = painterResource(R.drawable.dollar_logo_bottombar),
+                    contentDescription = "Home logo",
+                    modifier = Modifier.size(64.dp)
+                )
+            }
+            IconButton(
+                onClick = { navController.navigate("mapasYRutasScreen")},
+                modifier = Modifier.weight(1f)
+            ) {
+                Image(
+                    painter = painterResource(R.drawable.mapa_logo_bottombar),
+                    contentDescription = "Home logo",
+                    modifier = Modifier.size(64.dp)
+                )
+            }
+        }
+    }
+}
+
+// Función para crear la pantalla de inicio
 @Composable
 fun WelcomeScreen (modifier: Modifier = Modifier) {
     Surface(
         modifier = modifier.fillMaxSize(),
-        color = TealLight) {
+        color = TealLight
+    ) {
+        Box {
+            Image(
+                painter = painterResource(id = R.drawable.van_background_welcome),
+                contentDescription = "Van trip background",
+                modifier = Modifier.fillMaxSize(),
+                contentScale = ContentScale.Crop
+            )
+        }
+        Column (
+            modifier = Modifier.padding(12.dp),
+            verticalArrangement = Arrangement.spacedBy(8.dp)
+        ) {
+            Text(
+                text = stringResource(id = R.string.userGreeting),
+                modifier = Modifier.padding(12.dp)
+            )
+
             Text(
                 text = stringResource(id = R.string.welcome_message),
                 modifier = Modifier.padding(12.dp)
             )
+        }
+    }
+}
+
+// Función para crear la pantalla de conversión de divisas
+@Composable
+fun ConversorDivisasScreen (modifier: Modifier = Modifier) {
+    Surface (
+        modifier = modifier.fillMaxSize(),
+        color = MaterialTheme.colorScheme.background
+    ) {
+        Text(
+            text = "Hola conversor",
+            style = MaterialTheme.typography.titleMedium,
+            modifier = Modifier.padding(16.dp)
+        )
+    }
+}
+
+// Función para crear la pantalla de gastos de viaje
+@Composable
+fun GastosViajeScreen (modifier: Modifier = Modifier) {
+    Surface (
+        modifier = modifier.fillMaxSize(),
+        color = MaterialTheme.colorScheme.background
+    ) {
+        Text(
+            text = "Hola gastos de viaje",
+            style = MaterialTheme.typography.titleMedium,
+            modifier = Modifier.padding(16.dp)
+        )
+    }
+}
+
+// Función para crear la pantalla de mapas y rutas
+@Composable
+fun MapasYRutasScreen (modifier: Modifier = Modifier) {
+    Surface (
+        modifier = modifier.fillMaxSize(),
+        color = MaterialTheme.colorScheme.background
+    ) {
+        Text(
+            text = "Hola mapas y rutas",
+            style = MaterialTheme.typography.titleMedium,
+            modifier = Modifier.padding(16.dp)
+        )
     }
 }
 
@@ -112,6 +277,12 @@ fun  MyApp (modifier: Modifier) {
     WelcomeScreen(modifier)
 }
 
+
+@Composable
+fun MyTxt(text: String, modifier: Modifier) {
+    Text(text = text)
+
+}
 @Preview(showBackground = true)
 @Composable
 fun WelcomePreview() {
